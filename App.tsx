315661250c4afe3a1bcff2003e97e4e7c4a2c364
Raidel <raidelmfernandez@gmail.com>
2026-01-07 23:31:51 +0100

    comenzado App de cine y boton hecho

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import './global.css'
 
-import './global.css';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Boton } from 'components/Boton';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
-  );
+    <SafeAreaView className="flex-1">
+    <View className={"flex-1 justify-center items-center flex-column py-2 mx-8 mt-2"}>
+      <Boton texto={'Texto'} relleno={true}/>
+    </View>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({})
---
+++ components/Boton.tsx
@@ -0,0 +1,19 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import clsx from 'clsx';
+
+type BotonProps = {
+    texto:string
+    relleno:boolean
+    className?:string
+}
+
+export function Boton({texto,relleno,className}:BotonProps) {
+  return (
+    <Pressable className={clsx('px-6 py-3 rounded-lg', relleno ? 'bg-[#ff4f2e]' : 'bg-white border-2 border-[#ff4f2e]')}
+      onPress={() => console.log("hola")}>
+      <Text className={clsx('color-center font-semibold', relleno ? 'color-white' : "color-[#ff4f2e]", className)}>{texto}</Text>
+    </Pressable>
+  )
+}
+
